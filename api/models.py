1129a4e16a1b682f5df45c0f56d6eca35eb5e86b
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-04 11:43:39 +0300

    Implement custom User model with role, school, and bio; fix auth setup for Django

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('teacher','Teacher'),
+        ('student','Student')
+    ]
+    role = models.CharField(max_length=10,choices=ROLE_CHOICES,default="student")
+    school = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
