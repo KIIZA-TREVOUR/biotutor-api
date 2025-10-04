cfd1794df0fb035df3bf9662f9aa44ccc9267b07
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-04 12:13:51 +0300

    Added Category Model with name, description, slug and admin integration

--- api/models.py
+++ api/models.py
@@ -12,3 +12,13 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.role})"
+    
+# Category Model
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
