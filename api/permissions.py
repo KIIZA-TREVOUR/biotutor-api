b36a9c9a80aa24cd876fb1c1ee3fbf84c23d6e22
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 13:01:31 +0300

    Add custom permission classes: IsTeacher, IsAuthor for content ownership

---
+++ api/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+class IsTeacher(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'teacher'
+    
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'student'
+
+class IsAuthor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
