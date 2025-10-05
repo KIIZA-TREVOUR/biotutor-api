7cd29be9f10cc87b1b17bad1d71227bcec37d3e5
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 12:54:21 +0300

    Add BiologyContentSerializer with slug-based category and read-only author

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,19 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'name', 'description', 'slug', 'created_at']
 
+class BiologyContentSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Category.objects.all()
+    )
+    class Meta:
+        model = BiologyContent
+        fields = ['id', 'title', 'slug', 'content_body', 'summary', 'is_published', 'category', 'author', 'created_at', 'updated_at']
+        def create(self, validated_data):
+            return BiologyContent.objects.create(**validated_data)
+        
+        def update(self, instance, validated_data): 
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+            return instance
