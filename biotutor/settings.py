e9928e3aea5956c510efc9b73a1cb165a0102738
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 13:23:54 +0300

    Install django-filter and add to INSTALLED_APPS for content filtering

--- biotutor/settings.py
+++ biotutor/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'django_filters',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
