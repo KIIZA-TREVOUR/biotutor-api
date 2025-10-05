fd3b0a87e891667eb8717eace474dcb8f301df4c
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-10-05 13:22:21 +0300

    addurl routes for biology content list and detail endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'), 
+    path('content/', views.BiologyContentListView.as_view(), name='content-list'),
+    path('content/<int:pk>/', views.BiologyContentDetailView.as_view(), name='content-detail'),
 ]
